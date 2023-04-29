4266d5a802dcda94b07322613de97e0b40c0fcdf
Efe Kurban <efekurbann@gmail.com>
2023-04-29 15:33:49 +0300

    Use obliviate-utils

--- core/src/main/java/mc/obliviate/inventory/util/NMSUtil.java
+++ core/src/main/java/mc/obliviate/inventory/util/NMSUtil.java
@@ -1,13 +1,22 @@
 package mc.obliviate.inventory.util;
 
+import mc.obliviate.util.versiondetection.ServerVersionController;
 import org.bukkit.Bukkit;
 
 public class NMSUtil {
 
     public static final String NMS = getNMS();
-    public static final boolean IS_PAPER = Version.isPaper();
-    public static final Version VERSION = Version.getServerVersion(Bukkit.getServer());
-    public static final boolean CAN_USE_COMPONENTS = VERSION.isNewerOrSameThan(Version.v1_16_R3) && IS_PAPER;
+    public static final boolean IS_PAPER = isPaper();
+    public static final boolean CAN_USE_COMPONENTS = ServerVersionController.isServerVersionAtLeast(ServerVersionController.V1_16) && IS_PAPER;
+
+    private static boolean isPaper() {
+        try {
+            Class.forName("com.destroystokyo.paper.PaperConfig");
+            return true;
+        } catch (ClassNotFoundException ignored) {
+            return false;
+        }
+    }
 
     private static String getNMS() {
         final String version = Bukkit.getServer().getClass().getPackage().getName();
