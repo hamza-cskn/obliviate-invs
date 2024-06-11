93902b6bf3becc1201c7e96bbf36b87a7590fd4f
Deichor <deichors@gmail.com>
2024-06-12 00:41:52 +0300

    Update nms services

--- core/src/main/java/mc/obliviate/inventory/util/NMSUtil.java
+++ core/src/main/java/mc/obliviate/inventory/util/NMSUtil.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 
 public class NMSUtil {
 
-    public static final String NMS = getNMS();
     public static final boolean IS_PAPER = isPaper();
     public static final boolean CAN_USE_COMPONENTS = ServerVersionController.isServerVersionAtLeast(ServerVersionController.V1_16) && IS_PAPER;
     public static final LegacyComponentSerializer LEGACY = LegacyComponentSerializer.builder()
@@ -15,26 +14,19 @@ public class NMSUtil {
     private static boolean isPaper() {
         try {
             Class.forName("com.destroystokyo.paper.PaperConfig");
-            Bukkit.getLogger().info("Paper detected");
             return true;
         } catch (ClassNotFoundException ignored) {
-            Bukkit.getLogger().info("Paper detected");
             return false;
         }
     }
 
-    private static String getNMS() {
-        final String version = Bukkit.getServer().getClass().getPackage().getName();
-        Bukkit.getLogger().info("version" + version);
-        return version.substring(version.lastIndexOf('.') + 1);
+    private static final String CRAFTBUKKIT_PACKAGE = Bukkit.getServer().getClass().getPackage().getName();
+    public static String cbClass(String clazz) {
+        return (CRAFTBUKKIT_PACKAGE + "." + clazz);
     }
 
     public static Class<?> getCraftBukkitClass(final String name) throws ClassNotFoundException {
-        Class cl = Class.forName("org.bukkit.craftbukkit." + NMS + "." + name);
-        Bukkit.getLogger().info(Bukkit.getServer().getClass().getPackage().getName());
-        Bukkit.getLogger().info("clye bak: " + cl);
-        Bukkit.getLogger().info("versiona bak: " + NMS);
-        return Class.forName("org.bukkit.craftbukkit." + NMS + "." + name);
+        return Class.forName(cbClass(name));
     }
 
 }
